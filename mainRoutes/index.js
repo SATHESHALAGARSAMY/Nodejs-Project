219d920864dda6ede73f0222e0bd98541aefd16f
SATHESHALAGARSAMY <141311307+SATHESHALAGARSAMY@users.noreply.github.com>
2025-10-06 23:27:51 +0530

    Update index.js

--- mainRoutes/index.js
+++ mainRoutes/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const router = express.Router();
+const account = require("../modules/routes/accountroutes");
+const destination = require("../modules/routes/destinationroutes");
+const user = require("../modules/routes/userroutes");
+const log = require("../modules/routes/logsRoutes");
+const accountMember = require("../modules/routes/accountMemberRoutes");
+const dataHandler = require("../modules/routes/dataHandlerRoutes");
 
-var router = express.Router();
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Define routes
+router.use('/account', account);
+router.use('/destination', destination);
+router.use('/user', user);
+router.use('/log', log);
+router.use('/account-member', accountMember);
+router.use('/server', dataHandler);
 
+// Export the router
 module.exports = router;
