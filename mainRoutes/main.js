b088df7fa281eec6439a192125052f07b9654b99
SATHESHALAGARSAMY <141311307+SATHESHALAGARSAMY@users.noreply.github.com>
2025-10-06 23:28:43 +0530

    Update main.js

--- mainRoutes/main.js
+++ mainRoutes/main.js
@@ -5,10 +5,16 @@ const account = require("../modules/routes/accountroutes");
 const destination = require("../modules/routes/destinationroutes");
 const user = require("../modules/routes/userroutes");
 const log = require("../modules/routes/logsRoutes");
-// Define the POST route for creating an account
+const accountMember = require("../modules/routes/accountMemberRoutes");
+const dataHandler = require("../modules/routes/dataHandlerRoutes");
+
+// Define routes
 router.use('/account', account);
 router.use('/destination', destination);
 router.use('/user', user);
 router.use('/log', log);
+router.use('/account-member', accountMember);
+router.use('/server', dataHandler);
+
 // Export the router
 module.exports = router;
